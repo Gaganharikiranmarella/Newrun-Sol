3cdb41fd5541d394ff67a4bc182d92611e379fe4
Gaganharikiranmarella <ghk7125@gmail.com>
2025-05-21 20:26:16 +0530

    modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <h1>Newrun Solutions</h1>
-     <h2>Where Curiosity merges with Innovation</h2>
+      <div>
+        <header>
+          <h1>Newrun Solutions</h1>
+          <h2>Where Curiosity merges with Innovation</h2>
+        </header>
+
+        <main>
+
+        </main>
+
+        <footer>
+          <p>&copy; Newrun Solutions, All Rights Reserved</p>
+
+        </footer>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
