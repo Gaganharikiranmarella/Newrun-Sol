c2e40585a428ac1b08ce5f5f05e0ded50106d313
Gaganharikiranmarella <ghk7125@gmail.com>
2025-05-21 20:57:02 +0530

    added browser router

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,34 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
+    <div>
+      <BrowserRouter>
         <header>
-          <h1>Newrun Solutions</h1>
-          <h2>Where Curiosity merges with Innovation</h2>
+          <h1>My Online Shop</h1>
+          <Link to="/">Home</Link>-
+          <Link to="/cart">Cart</Link>
           <hr />
         </header>
 
         <main>
-          <h1>Projects List</h1>  
+          <Routes>
+            <Route index element={<Product />} />
+            <Route path="/" element={<Product/>}></Route>
+            <Route path="/cart" element={<Cart/>}></Route>
+          </Routes>
         </main>
 
         <footer>
           <hr />
-          <p>&copy; Newrun Solutions, All Rights Reserved</p>
+          &copy; 2005. All rights Reserved.
         </footer>
-      </div>
-    </>
+      </BrowserRouter>
+    </div>
   );
 }
-
 export default App;
