0d7e780535049455a22b651c95c8fce477cef2c7
Gaganharikiranmarella <ghk7125@gmail.com>
2025-06-01 18:13:40 +0530

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "./assets/logo.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Product from "./components/Product";
@@ -32,7 +32,7 @@ function App() {
 
         <footer>
           <hr />
-          &copy; 2005. All rights Reserved.
+          &copy;Gagan Hari Kiran Marella; 2025. All rights Reserved.
         </footer>
       </BrowserRouter>
     </div>
