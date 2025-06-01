72e1e8b0415600aff97e7441754c1dcd230fec9b
Gaganharikiranmarella <ghk7125@gmail.com>
2025-06-01 18:18:03 +0530

    modified register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Register() {
   return (
-    <div>
+    <div className="form-container">
       <h2>Register</h2>
       <input type="text" placeholder="Name (optional)" /><br />
       <input type="email" placeholder="Email" required /><br />
@@ -16,6 +17,14 @@ export default function Register() {
       <input type="password" placeholder="Password" required /><br />
       <input type="password" placeholder="Confirm Password" required /><br />
       <button>Register</button>
+
+      {/* Link to Login */}
+      <p style={{ marginTop: '1rem', textAlign: 'center' }}>
+        Already have an account?{' '}
+        <Link to="/login" style={{ color: '#ff6f61', textDecoration: 'underline' }}>
+          Login
+        </Link>
+      </p>
     </div>
   );
 }
